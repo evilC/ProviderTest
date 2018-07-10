40c943126a25f2cadb6ccf6dbf57db3bf008248f
evilC <evilc@evilc.com>
2018-07-10 22:43:39 +0100

    Add EventArgs

---
+++ ProviderTest 001/Common/EventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class InputReportEventArgs : EventArgs
+    {
+        public InputModeReport InputModeReport { get; }
+
+        public InputReportEventArgs(InputModeReport inputModeReport)
+        {
+            InputModeReport = inputModeReport;
+        }
+    }
+
+    public class DeviceEmptyEventArgs : EventArgs
+    {
+        public DeviceDescriptor DeviceDescriptor { get; }
+
+        public DeviceEmptyEventArgs(DeviceDescriptor deviceDescriptor)
+        {
+            DeviceDescriptor = deviceDescriptor;
+        }
+    }
+}
